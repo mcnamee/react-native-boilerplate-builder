924d78cbb5f249e925b3d6e6c6a3208c1ede3cb9
Matt McNamee <matt@mcnamee.co>
2019-11-21 08:43:35 +1000

    Fix tests, make async actually run

--- OURS/src/tests/models/articles.test.js
+++ OURS/src/tests/models/articles.test.js
@@ -44,12 +44,12 @@ const mockOutput = {
 /**
  * Tests
  */
-it('Articles fetchList() returns correctly', () => {
+it('Articles fetchList() returns correctly', async () => {
   Api.get.mockResolvedValue({ data: [mockInput], headers: { 'x-wp-totalpages': 3 } });
   const initialState = { articles: { lastSync: {} } };
   const dispatch = { articles: { replace: jest.fn((res) => res) } };
 
-  model.effects(dispatch).fetchList({ page: 2 }, initialState).then((res) => {
+  await model.effects(dispatch).fetchList({ page: 2 }, initialState).then((res) => {
     expect(Api.get).toHaveBeenCalledWith('/v2/posts?per_page=4&page=2&orderby=modified&_embed');
     expect(dispatch.articles.replace).toHaveBeenCalledTimes(1);
     expect(res).toEqual({
@@ -60,15 +60,15 @@ it('Articles fetchList() returns correctly', () => {
   });
 });
 
-it('Articles fetchList() does not go to API if lastSync just set', () => {
+it('Articles fetchList() does not go to API if lastSync just set', async () => {
   const initialState = { articles: { lastSync: { 2: moment() } } };
-  model.effects().fetchList({ page: 2 }, initialState).then((res) => expect(res).toEqual(true));
+  await model.effects().fetchList({ page: 2 }, initialState).then((res) => expect(res).toEqual(true));
 });
 
-it('Articles fetchSingle() returns correctly', () => {
+it('Articles fetchSingle() returns correctly', async () => {
   Api.get.mockResolvedValue({ data: mockInput });
 
-  model.effects().fetchSingle(222).then((res) => {
+  await model.effects().fetchSingle(222).then((res) => {
     expect(Api.get).toHaveBeenCalledWith('/v2/posts/222?_embed');
     expect(res).toEqual(mockOutput);
   });
